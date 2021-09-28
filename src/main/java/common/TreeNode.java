8bff0da129b1ae0fb6153038893ae2fd07f5f51a
svher <svhero658@gmail.com>
2021-09-28 10:31:00 +0800

    init commit

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,15 @@
+package common;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+}
