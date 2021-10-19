f42d553c0e2699d1deb28bbf94e256900ec3e4c5
svher <svhero658@gmail.com>
2021-10-19 21:17:50 +0800

    unique paths (combination calc notes)

--- src/main/java/unique_paths/Solution_1.java
+++ src/main/java/unique_paths/Solution_1.java
@@ -6,8 +6,10 @@ public class Solution_1 implements Solution {
     public int uniquePaths(int m, int n) {
         m--;
         n--;
+        // 这里不用 long 会溢出
         long res = 1;
-        for (int i = m + 1, j = 1; i <= m + n; i++, j++) {
+        // 公式为 (n + m)! / (n! * m!)，这里 i 选择 m + 1 或者 n + 1 都可以
+        for (int i = n + 1, j = 1; i <= m + n; i++, j++) {
             // IMPORTANT FOR THIS TO WORK:
             // if we divide res by j, we already multiplied res by j consecutive numbers,
             // so we must have a number which is multiple of j
