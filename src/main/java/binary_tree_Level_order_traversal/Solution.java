040fe48a40f0b3dfe857024b12f60ecfc228fa19
svher <svhero658@gmail.com>
2021-10-19 21:17:50 +0800

    binary tree level order traversal (1)

---
+++ src/main/java/binary_tree_Level_order_traversal/Solution.java
@@ -0,0 +1,4 @@
+package binary_tree_Level_order_traversal;
+
+public interface Solution {
+}
---
+++ src/main/java/binary_tree_Level_order_traversal/Solution_1.java
@@ -0,0 +1,35 @@
+package binary_tree_Level_order_traversal;
+
+import common.TreeNode;
+
+import java.util.*;
+
+class Solution_1 implements Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> levelNodes = new ArrayList<>();
+        queue.offer(root);
+        int cnt = 1;
+        while (!queue.isEmpty()) {
+            TreeNode top = queue.poll();
+            levelNodes.add(top.val);
+            if (top.left != null) {
+                queue.offer(top.left);
+            }
+            if (top.right != null) {
+                queue.offer(top.right);
+            }
+            if (--cnt == 0) {
+                ans.add(levelNodes);
+                levelNodes = new ArrayList<>();
+                cnt = queue.size();
+            }
+        }
+        return ans;
+    }
+}
