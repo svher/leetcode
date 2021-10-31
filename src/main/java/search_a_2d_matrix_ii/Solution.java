6b47d0a72b43f770a78cf953050e09c656d0bda7
svher <svhero658@gmail.com>
2021-10-31 21:36:54 +0800

    search a 2d matrix ii (1: O(m+n) from top right corner)

---
+++ src/main/java/search_a_2d_matrix_ii/Solution.java
@@ -0,0 +1,5 @@
+package search_a_2d_matrix_ii;
+
+public interface Solution {
+    boolean searchMatrix(int[][] matrix, int target);
+}
---
+++ src/main/java/search_a_2d_matrix_ii/Solution_1.java
@@ -0,0 +1,20 @@
+package search_a_2d_matrix_ii;
+
+public class Solution_1 implements Solution {
+
+    @Override
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
